79ad35c70fb22d8522cf5eb93024bab9d93a63a6
Runiloo <ldi199119@gmail.com>
2017-09-23 18:55:23 +0300

    Text downloader needed

--- src/main/java/forGUI/mainGUI.java
+++ src/main/java/forGUI/mainGUI.java
@@ -11,6 +11,7 @@ import java.awt.event.*;
 import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.ArrayList;
 
 
 class mainGUI extends Container implements Runnable {
@@ -46,12 +47,12 @@ class mainGUI extends Container implements Runnable {
         searchButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 searchRun();
-                }
+            }
         });
         //CHOSE EXTEND OF FILE
         comboBox1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-            item = comboBox1.getSelectedItem().toString();
+                item = comboBox1.getSelectedItem().toString();
             }
         });
         //TREE
@@ -69,11 +70,11 @@ class mainGUI extends Container implements Runnable {
         tree.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
-                if(SwingUtilities.isRightMouseButton(e)){
+                if (SwingUtilities.isRightMouseButton(e)) {
                     int selRow = tree.getRowForLocation(e.getX(), e.getY());
                     TreePath selPath = tree.getPathForLocation(e.getX(), e.getY());
                     tree.setSelectionPath(selPath);
-                    if (selRow>-1){
+                    if (selRow > -1) {
                         tree.setSelectionRow(selRow);
                     }
                     JTextArea textArea2 = new JTextArea();
@@ -135,7 +136,7 @@ class mainGUI extends Container implements Runnable {
         frame.setVisible(true);
     }
 
-    public static void main(String[] args)throws InterruptedException  {
+    public static void main(String[] args) throws InterruptedException {
         SwingUtilities.invokeLater(new mainGUI());
 
         try {
@@ -151,20 +152,20 @@ class mainGUI extends Container implements Runnable {
         }
     }
 
-    private void explorerRun(){
+    private void explorerRun() {
         JFileChooser dialog = new JFileChooser();
         dialog.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
         dialog.setFileFilter(new MyFileFilter(item));
 
         int ret = dialog.showDialog(null, "OK");
-        if(ret == JFileChooser.APPROVE_OPTION) {
+        if (ret == JFileChooser.APPROVE_OPTION) {
             pathTextField.setText(dialog.getSelectedFile().getAbsolutePath());
-            searchPath =  dialog.getSelectedFile();
+            searchPath = dialog.getSelectedFile();
         }
     }
 
-    private void searchRun(){
-        if(searchRequest==null||searchRequest.equals(""))
+    private void searchRun() {
+        if (searchRequest == null || searchRequest.equals(""))
             JOptionPane.showMessageDialog(null, "¬ведите поисковый запрос");
         else if (searchPath != null && searchPath.exists()) {
             textArea1.setText(null);
@@ -173,23 +174,101 @@ class mainGUI extends Container implements Runnable {
             tree.setModel(treeModel);
             scrollPane.setViewportView(tree);
 
-            CreateChildNodes ccn = new CreateChildNodes(searchPath, root, item, treeModel,tree, searchRequest);
+            CreateChildNodes ccn = new CreateChildNodes(searchPath, root, item, treeModel, tree, searchRequest);
             ccn.run();
             tree.setCellRenderer(new MyRenderer(UIManager.getIcon("OptionPane.informationIcon")));
-        }
-        else JOptionPane.showMessageDialog(null, "”казанна€ директори€ не существует");
+        } else JOptionPane.showMessageDialog(null, "”казанна€ директори€ не существует");
     }
 
-    private void openFile(JTree tree, JTextArea textArea) throws IOException {
-        FileReader file_reader = null;
-        File fReader = null;
+    private void openFile(JTree tree, final JTextArea textArea) throws IOException {
+        FileReader fileReader = null;
+        File file = null;
+        BufferedReader[] bufferedReader;
         textArea.setEditable(false);
-        if (tree.getSelectionPath()!=null) {
-            fReader = new File(tree.getSelectionPath().getLastPathComponent().toString());
+        if (tree.getSelectionPath() != null) {
+            file = new File(tree.getSelectionPath().getLastPathComponent().toString());
         }
-        if(fReader!=null&&fReader.isFile()) {
-            FileInputStream f = new FileInputStream( fReader );
+        if (file != null && file.isFile()) {
+            FileInputStream f = new FileInputStream( file );
             FileChannel ch = f.getChannel( );
+            ArrayList<ByteBuffer> byteBuffer = new ArrayList<ByteBuffer>();
+
+            int nRead, nGet, j=0;
+            textArea.setText(null);
+            ByteBuffer buffer = ByteBuffer.allocate( 409600 );
+            while ( (nRead=ch.read( buffer )) != -1 ) {
+                if (nRead == 0)
+                    continue;
+                byteBuffer.add(buffer);
+            }
+            ByteBuffer byteBuffer1 = ByteBuffer.allocate( 409600 ;
+            for(int ff = 0; ff<2;ff++) {
+                byteBuffer1 = byteBuffer.get(ff);
+
+                byteBuffer1.position(0);
+                byteBuffer1.limit(nRead);
+                while (byteBuffer.hasRemaining()) {
+                    nGet = Math.min(byteBuffer.remaining(), 128);
+                    byteBuffer.get(byteArray, 0, nGet);
+                    textArea.append(new String(byteArray, "windows-1251"));
+                }
+                byteBuffer.clear();
+            }
+            byte[] byteArray = new byte[128];
+            byteBuffer.get(0).get()
+
+
+
+
+            /*while ( (nRead=ch.read( byteBuffer )) != -1 ) {
+                if (nRead == 0)
+                    continue;
+                byteBuffer.position(0);
+                byteBuffer.limit(nRead);
+                while (byteBuffer.hasRemaining()) {
+                    nGet = Math.min(byteBuffer.remaining(), 128);
+                    byteBuffer.get(byteArray, 0, nGet);
+                    textArea.append(new String(byteArray, "windows-1251"));
+                }
+                byteBuffer.clear();
+
+            }*/
+    }
+}
+
+
+
+
+
+
+
+    /*fileReader = new FileReader(file);
+
+            for(int i = 0; i< 100; i++) {
+        textArea.append(bufferedReader.readLine());
+        textArea.append("\n");
+    }
+}
+    final BufferedReader finalBufferedReader = bufferedReader;
+        textArea.addMouseWheelListener(new MouseWheelListener() {
+public void mouseWheelMoved(MouseWheelEvent e) {
+        int i = e.getWheelRotation();
+        if(i==-1&&finalBufferedReader!=null) {
+        try {
+        for(int j = 0; j< 10; j++) {
+        textArea.append(finalBufferedReader.readLine());
+        textArea.append("\n");
+        }
+        } catch (IOException e1) {
+        e1.printStackTrace();
+        }
+        }*/
+
+
+
+            /*FileInputStream f = new FileInputStream( file );
+            FileChannel ch = f.getChannel( );
+            ch.re
             ByteBuffer byteBuffer = ByteBuffer.allocate( 1024 );
             byte[] byteArray = new byte[128];
             int nRead, nGet, j=0;
@@ -206,10 +285,10 @@ class mainGUI extends Container implements Runnable {
                     byteBuffer.get( byteArray, 0, nGet );
                     textArea.append(new String(byteArray, "windows-1251"));
                 }
-                byteBuffer.clear( );
-            }
+                byteBuffer.clear( );*/
+
             /*try {
-                file_reader = new FileReader(fReader);
+                fileReader = new FileReader(file);
             } catch (FileNotFoundException e1) {
                 e1.printStackTrace();
             }
@@ -217,7 +296,7 @@ class mainGUI extends Container implements Runnable {
             char buffer[] = new char[4096];
             int len;
             try {
-                while ((len = file_reader.read(buffer)) != -1) {
+                while ((len = fileReader.read(buffer)) != -1) {
                     String s = new String(buffer, 0, len);
                     textArea.append(s);
                 }
@@ -225,11 +304,11 @@ class mainGUI extends Container implements Runnable {
                 e1.printStackTrace();
             }
             try {
-                file_reader.close();
+                fileReader.close();
             } catch (IOException e1) {
                 e1.printStackTrace();
             }
             textArea.setCaretPosition(0);*/
-        }
     }
 }
+
